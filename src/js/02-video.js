a1999f4e688dd59a3c8fe29e8069f75f910a5c52
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-07-03 18:41:16 +0300

    gh-pages

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 import Player from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+const storedTime = localStorage.getItem('videoplayer-current-time');
+if (storedTime) {
+    player.setCurrentTime(parseFloat(storedTime));
+  }
+  const updateStoredTime = throttle(() => {
+    player.getCurrentTime().then((currentTime) => {
+      localStorage.setItem('videoplayer-current-time', currentTime);
+    });
+  }, 1000);
+  player.on('timeupdate', updateStoredTime);
+  console.log(player);  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+
+const storageKey = 'feedback-form-state';
+
+
+const saveFormState = () => {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem(storageKey, JSON.stringify(state));
+};
+
+
+const handleInputChange = throttle(saveFormState, 500);
+
+
+emailInput.addEventListener('input', handleInputChange);
+messageInput.addEventListener('input', handleInputChange);
+
+
+const loadFormState = () => {
+  const stateJSON = localStorage.getItem(storageKey);
+  if (stateJSON) {
+    const state = JSON.parse(stateJSON);
+    emailInput.value = state.email;
+    messageInput.value = state.message;
+  }
+};
+
+
+window.addEventListener('load', loadFormState);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+ 
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+
+  
+  emailInput.value = '';
+  messageInput.value = '';
+
+ 
+  localStorage.removeItem(storageKey);
+
+
+  console.log(state);
+});
